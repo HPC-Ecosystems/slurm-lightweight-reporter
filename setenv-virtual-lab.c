b7e9f914b2f832f510ce294e7df0b881f4bac869
Bryan Johnston <brattex@gmail.com>
2022-07-26 11:58:47 +0200

    updated setenv

--- setenv-virtual-lab.c
+++ setenv-virtual-lab.c
@@ -4,3 +4,6 @@ sms_ip=10.10.10.10 	# Internal IP address on SMS server
 # chrony time server information
 ntp_server=time.google.com
 cluster_ip_range=10.10.10.0/24
+
+sms_eth_internal=eth1 # Internal Ethernet interface on SMS
+internal_netmask=255.255.255.0 # Subnet netmask for internal network
