24e59c0cdf332121ad9cbfd174f4ba51f9c9b4c5
Lara Timm <ltimm@csir.co.za>
2022-07-26 09:40:24 +0000

    set env var for chrony conf to limit ips

--- setenv-virtual-lab.c
+++ setenv-virtual-lab.c
@@ -3,3 +3,4 @@ sms_name=sms-host 	# Hostname for SMS server
 sms_ip=10.10.10.10 	# Internal IP address on SMS server
 # chrony time server information
 ntp_server=time.google.com
+cluster_ip_range=10.10.10.0/24
